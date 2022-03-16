7ca1d613f8e184a512349a7bf3ebc06ac9c273e1
Programistyczny Świr <programistycznyswir@gmail.com>
2022-03-16 16:36:23 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MaszynaLosujaca _maszynaLosujaca;
+        Model model;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = new Model();
+            this.DataContext = model;
+            _maszynaLosujaca = new MaszynaLosujaca();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Wiem że w tym miejscu lepiej by było zrobić 2 metody, ale zrobiłem tak, bo czemu nie.
+            if (sender is Button button)
+                switch (button.Name)
+                {
+                    case "Button_In":
+                        _maszynaLosujaca.Add(model.TextBox_Text);
+                        //MessageBox.Show(model.TextBox_Text);
+                        model.TextBox_Text = "";
+                        break;
+                    case "Button_Out":
+                        if (_maszynaLosujaca.AreThereAnyWords is false)
+                            MessageBox.Show("Nie ma żadnych losów w maszynie!");
+                        else
+                            model.Label_Text = _maszynaLosujaca.GetRandomWord();
+                        break;
+                    default:
+                        Console.Error.WriteLine("This event sender is not mapped!!");
+                        break;
+                }
+            this.DataContext = null;
+            this.DataContext = model;
+        }
+    }
+
+    class Model
+    {
+        public string TextBox_Text { get; set; } = "Los";
+        public string Label_Text { get; set; } = "...";
+
+    }
+}
---
+++ MaszynaLosujaca.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class MaszynaLosujaca
+    {
+        List<string> Words = new List<string>();
+        readonly Random _rng = new Random();
+
+        public MaszynaLosujaca Add(string word)
+        {
+            Words.Add(word);
+            return this;
+        }
+
+        public string GetRandomWord()
+            => Words.PopAt(_rng.Next(Words.Count));
+
+        public bool AreThereAnyWords
+            => Words.Count > 0;
+
+        public List<string> GetAllWords()
+            => Words;
+    }
+
+    static class List_Extensions
+    {
+        public static T PopAt<T>(this List<T> self, int index)
+        {
+            T result = self[index];
+            self.RemoveAt(index);
+            return result;
+        }
+    }
+}
